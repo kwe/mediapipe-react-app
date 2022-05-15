7be7ef3758162b117edb9c51bd530c25a6e25613
Kevin Evans <kwevans@gmail.com>
2022-05-15 21:05:48 +0100

    add Webcam

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
 import './App.css';
+import Webcam from 'react-webcam';
 
 const App = () => {
   return (
     <div className="App">
       <h1 className="container text-3xl font-bold underline">Hello world!</h1>
+			<p><Webcam/></p>
     </div>
   );
 };
