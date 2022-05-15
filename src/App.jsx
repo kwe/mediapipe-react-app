c50d27ebe4fda971bd28ee3bea6f51710c4a2ab6
Kevin Evans <kwevans@gmail.com>
2022-05-15 21:00:57 +0100

    initia commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="container text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
+
+export default App;
